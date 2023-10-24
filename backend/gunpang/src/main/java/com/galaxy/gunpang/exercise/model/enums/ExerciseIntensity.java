4b9ac049f0b1ee43c2320d448426d9e90573a006
박수빈 <jrkd2165@gmail.com>
2023-10-24 14:42:26 +0900

    [3주차/BE] exerciseRepository 생성

--- backend/gunpang/src/main/java/com/galaxy/gunpang/exercise/model/enums/ExerciseIntensity.java
+++ backend/gunpang/src/main/java/com/galaxy/gunpang/exercise/model/enums/ExerciseIntensity.java
@@ -1,11 +1,11 @@
-package com.galaxy.gunpang.exercise.entity;
+package com.galaxy.gunpang.exercise.model.enums;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum ExerciseEnum {
+public enum ExerciseIntensity {
     HIGH("고"), MEDIUM("중"), LOW("저"), NOT("운동이 아님");
 
     private final String value;
