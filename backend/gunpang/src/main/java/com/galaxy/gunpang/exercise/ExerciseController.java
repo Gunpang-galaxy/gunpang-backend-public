513005efd1b9b5badc0be92361b8b6902c9a62cf
박수빈 <jrkd2165@gmail.com>
2023-10-31 13:20:31 +0900

    [4주차/BE] 운동 기록하기 토큰에서 userId 가져오는 것으로 수정

--- backend/gunpang/src/main/java/com/galaxy/gunpang/exercise/ExerciseController.java
+++ backend/gunpang/src/main/java/com/galaxy/gunpang/exercise/ExerciseController.java
@@ -3,6 +3,7 @@ package com.galaxy.gunpang.exercise;
 
 import com.galaxy.gunpang.exercise.model.dto.ExerciseRecordReqDto;
 import com.galaxy.gunpang.exercise.service.ExerciseService;
+import com.galaxy.gunpang.user.service.UserService;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class ExerciseController {
 
     private final ExerciseService exerciseService;
+    private final UserService userService;
 
 
     @ApiResponses(value = {
@@ -26,9 +28,9 @@ public class ExerciseController {
             @ApiResponse(responseCode = "500", description = "서버 오류")
     })
     @PostMapping(consumes = "application/json;charset=UTF-8")
-    public ResponseEntity<?> recordExercise(@RequestBody ExerciseRecordReqDto exerciseRecordReqDto) throws Exception {
-
-        exerciseService.recordExercise(exerciseRecordReqDto.getUserId(),exerciseRecordReqDto.getStartedTime(),exerciseRecordReqDto.getFinishedTime(),exerciseRecordReqDto.getExerciseIntensity());
+    public ResponseEntity<?> recordExercise(@RequestHeader("Authorization") String token, @RequestBody ExerciseRecordReqDto exerciseRecordReqDto) throws Exception {
+        Long userId = userService.getIdByToken(token).getId();
+        exerciseService.recordExercise(userId,exerciseRecordReqDto.getStartedTime(),exerciseRecordReqDto.getFinishedTime(),exerciseRecordReqDto.getExerciseIntensity());
 
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
