2904830d75cded6e81e88c15c7497a1ebdc4028f
박수빈 <jrkd2165@gmail.com>
2023-11-02 16:48:48 +0900

    [4주차/BE] style: api 설명 추가

--- backend/gunpang/src/main/java/com/galaxy/gunpang/notification/NotificationController.java
+++ backend/gunpang/src/main/java/com/galaxy/gunpang/notification/NotificationController.java
@@ -3,6 +3,7 @@ package com.galaxy.gunpang.notification;
 import com.galaxy.gunpang.notification.model.dto.RecordNotificationReqDto;
 import com.galaxy.gunpang.notification.model.dto.SendReqDto;
 import com.galaxy.gunpang.notification.service.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,15 +23,18 @@ public class NotificationController {
     @Autowired
     NotificationService notificationService;
     private static final Logger logger = LoggerFactory.getLogger(NotificationController.class);
+
+    @Operation(summary = "알림 보내기", description = "사용자에게 백그라운드 알림을 보냅니다.")
     @PostMapping(value = "/fcm/notification", consumes = "application/json;charset=UTF-8")
     public ResponseEntity sendNotification(@RequestBody SendReqDto sendReqDto) throws IOException {
 
         logger.debug("controller");
-        notificationService.sendNotification(sendReqDto.getTargetToken(), sendReqDto.getTitle(),sendReqDto.getBody());
+        notificationService.sendNotification(sendReqDto.getTargetToken(), sendReqDto.getTitle(), sendReqDto.getBody());
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping(value = "/fcm/notification/record", consumes = "application/json;charset=UTF-8" )
+    @Operation(summary = "디바이스 토큰 저장", description = "사용자의 디바이스 토큰을 저장합니다.")
+    @PostMapping(value = "/fcm/notification/record", consumes = "application/json;charset=UTF-8")
     public ResponseEntity recordNotification(@RequestBody RecordNotificationReqDto recordNotificationReqDto) throws IOException {
 
         logger.debug("controller");
