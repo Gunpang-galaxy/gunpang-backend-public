3e1d46de713dfc51e4f6aa58d5ae80f1c348a810
박수빈 <jrkd2165@gmail.com>
2023-11-02 16:18:47 +0900

    [4주차/BE] 체성분 api 설명 추가

--- backend/gunpang/src/main/java/com/galaxy/gunpang/bodyComposition/BodyCompositionController.java
+++ backend/gunpang/src/main/java/com/galaxy/gunpang/bodyComposition/BodyCompositionController.java
@@ -1,12 +1,10 @@
 package com.galaxy.gunpang.bodyComposition;
 
-import com.galaxy.gunpang.bodyComposition.model.BodyComposition;
 import com.galaxy.gunpang.bodyComposition.model.dto.BodyCompositionApiReqDto;
 import com.galaxy.gunpang.bodyComposition.model.dto.BodyCompositionResDto;
 import com.galaxy.gunpang.bodyComposition.service.BodyCompositionService;
-import com.galaxy.gunpang.dailyRecord.DailyRecordController;
 import com.galaxy.gunpang.user.service.UserService;
-import com.galaxy.gunpang.user.utils.JwtUtil;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
@@ -23,8 +21,8 @@ public class BodyCompositionController {
     private static final Logger logger = LoggerFactory.getLogger(BodyCompositionController.class);
     private final BodyCompositionService bodyCompositionService;
     private final UserService userService;
-    private final JwtUtil jwtUtil;
 
+    @Operation(summary = "체성분 저장하기", description = "삼성헬스에서 받아온 체성분 정보를 저장합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "체성분 기록 성공"),
             @ApiResponse(responseCode = "400", description = "잘못된 요청"),
@@ -43,6 +41,7 @@ public class BodyCompositionController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @Operation(summary = "체성분 기록 조회", description = "체성분 기록을 조회합니다.")
     @GetMapping("/")
     public ResponseEntity<?> getBodyCompositions(@RequestHeader("Authorization") String token){
         logger.debug("controller");
