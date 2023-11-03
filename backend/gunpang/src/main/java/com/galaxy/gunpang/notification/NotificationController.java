73b571830c28953d401d8cd4a5b0f850a7395b34
박수빈 <jrkd2165@gmail.com>
2023-11-03 10:53:43 +0900

    [4주차/BE] 알림 디바이스 토큰 저장시 userId 헤더에서 가져오는 걸로 변경

--- backend/gunpang/src/main/java/com/galaxy/gunpang/notification/NotificationController.java
+++ backend/gunpang/src/main/java/com/galaxy/gunpang/notification/NotificationController.java
@@ -3,6 +3,7 @@ package com.galaxy.gunpang.notification;
 import com.galaxy.gunpang.notification.model.dto.RecordNotificationReqDto;
 import com.galaxy.gunpang.notification.model.dto.SendReqDto;
 import com.galaxy.gunpang.notification.service.NotificationService;
+import com.galaxy.gunpang.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -12,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -20,8 +22,9 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class NotificationController {
 
-    @Autowired
-    NotificationService notificationService;
+    private final NotificationService notificationService;
+
+    private final UserService userService;
     private static final Logger logger = LoggerFactory.getLogger(NotificationController.class);
 
     @Operation(summary = "알림 보내기", description = "사용자에게 백그라운드 알림을 보냅니다.")
@@ -35,10 +38,10 @@ public class NotificationController {
 
     @Operation(summary = "디바이스 토큰 저장", description = "사용자의 디바이스 토큰을 저장합니다.")
     @PostMapping(value = "/fcm/notification/record", consumes = "application/json;charset=UTF-8")
-    public ResponseEntity recordNotification(@RequestBody RecordNotificationReqDto recordNotificationReqDto) throws IOException {
-
+    public ResponseEntity recordNotification(@RequestHeader("Authorization") String token, @RequestBody RecordNotificationReqDto recordNotificationReqDto) throws IOException {
+        Long userId = userService.getIdByToken(token).getId();
         logger.debug("controller");
-        notificationService.recordNotification(recordNotificationReqDto.getToken(), recordNotificationReqDto.getUserId());
+        notificationService.recordNotification(recordNotificationReqDto.getToken(), userId);
         return new ResponseEntity<>(HttpStatus.CREATED);
 
     }
