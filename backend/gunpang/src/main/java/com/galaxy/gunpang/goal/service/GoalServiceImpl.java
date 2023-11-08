3cb1304197fe8825bbfe351fd5e997281b7e0e57
양우철 <wcyang8@naver.com>
2023-11-08 12:46:40 +0900

    [5주차/BE] fix: GoalAlreadyExistException 처리

--- backend/gunpang/src/main/java/com/galaxy/gunpang/goal/service/GoalServiceImpl.java
+++ backend/gunpang/src/main/java/com/galaxy/gunpang/goal/service/GoalServiceImpl.java
@@ -1,11 +1,13 @@
 package com.galaxy.gunpang.goal.service;
 
+import com.galaxy.gunpang.avatar.exception.AvatarAlreadyExistException;
 import com.galaxy.gunpang.avatar.exception.AvatarNotFoundException;
 import com.galaxy.gunpang.avatar.model.Avatar;
 import com.galaxy.gunpang.avatar.model.dto.AvatarIdReqDto;
 import com.galaxy.gunpang.avatar.repository.AvatarRepository;
 import com.galaxy.gunpang.avatar.repository.DeathCauseRepository;
 import com.galaxy.gunpang.common.model.enums.InitCode;
+import com.galaxy.gunpang.goal.exception.GoalAlreadyExistException;
 import com.galaxy.gunpang.goal.exception.GoalNotFoundException;
 import com.galaxy.gunpang.goal.model.Goal;
 import com.galaxy.gunpang.goal.model.dto.CalendarResDto;
@@ -35,6 +37,12 @@ public class GoalServiceImpl implements GoalService{
         Avatar avatar = avatarRepository.findById(avatarId).orElseThrow(
                 AvatarNotFoundException::new
         );
+
+        // 임시로 직접 repo에서 중복 avatarId 체크
+        if(goalRepository.findByAvatar_Id(avatarId).isPresent()){
+            throw new GoalAlreadyExistException(avatarId);
+        }
+
         Goal goal = Goal.builder().avatar(avatar)
                 .exerciseDay((byte)goalReqDto.getExerciseDay())
                 .exerciseTime(goalReqDto.getExerciseTime())
