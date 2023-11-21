703d7b57b54e05e843085c2a3ccf0a08468c3ff6
박예한 <parkyehan2060@gmail.com>
2023-11-21 12:56:44 +0900

    [9주차/BE]: 운동 강도 별 유지 시간 저장

--- backend/gunpang/src/main/java/com/galaxy/gunpang/exercise/service/ExerciseService.java
+++ backend/gunpang/src/main/java/com/galaxy/gunpang/exercise/service/ExerciseService.java
@@ -8,4 +8,7 @@ public interface ExerciseService {
 
     public void recordExercise(Long userId, LocalDateTime startedTime, LocalDateTime finishedTime, ExerciseIntensity exerciseIntensity) throws Exception;
 
+    // 운동 완료 후 flask 서버에 운동 기록(심박수, 시간) 요청
+    public void exerciseComplete(Long userId) throws Exception;
+
 }
